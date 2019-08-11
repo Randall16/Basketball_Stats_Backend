a247e89dc50cbc171e8e1cde845253ce7d5a359b
Randall16 <randallgr5@outlook.com>
2019-08-11 16:44:36 -0400

    restructure db scripts

---
+++ populate_db.py
@@ -0,0 +1,18 @@
+import mysql.connector
+import time
+
+from parse_player_season import get_player_seasons
+from parse_players import get_players
+from models import *
+from database import *
+
+
+for i in range(1990, 1980, -1):
+    time.sleep(14)
+    seasons = get_player_seasons(i, False)
+    cursor.executemany(SQL_INSERT_SEASON_TABLE, seasons)
+    mydb.commit()
+    print('%d year successfully inserted' % i)
+
+
+mydb.close()
--- run.py
+++ run.py
@@ -6,25 +6,23 @@ from parse_players import get_players
 from models import *
 from database import *
 
-import time
+''' Use this file to run sample queries against db '''
 
-""" for i in range(2010, 2000, -1):
-    time.sleep(26)
-    seasons = get_player_seasons(i, False)
-    cursor.executemany(SQL_INSERT_SEASON_TABLE, seasons)
-
-mydb.commit()
-
-mydb.close() """
-
-s = '''player_id, year, 
+cols = '''season_table.player_id, year, 
     playoffs, team, age, games_played, games_started, minutes, field_goals_made,
     field_goals_attempted, threes_made, threes_attempted, twos_made,
     twos_attempted, free_throws_made, free_throws_attempted, offensive_rebounds,
     defensive_rebounds, total_rebounds, assists, steals, blocks, turnovers,
     personal_fouls, points'''
 
-cursor.execute('select %s from season_table where player_id = \'jamesle01\'' % s)
+sql_query = ''' SELECT %s
+                FROM season_table
+                INNER JOIN player_table 
+                ON season_table.player_id = player_table.player_id
+                WHERE player_table.name = \'%s\' '''
+
+#print(sql_query % (cols, 'Larry Bird'))
+cursor.execute(sql_query % (cols, 'Michael Jordan'))
 seasons = [ PlayerSeasonTotals(*i) for i in cursor.fetchall() ]
 
 for i in seasons:
